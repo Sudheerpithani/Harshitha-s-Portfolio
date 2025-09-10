e2958955bc84c6e99c3b789e912f79893ca257ef
Sudheerpithani <sudheerpithani143@gmail.com>
2025-09-10 16:05:56 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",          // root HTML file
+    "./src/**/*.{ts,tsx,js,jsx}", // all JS/TS/React files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
